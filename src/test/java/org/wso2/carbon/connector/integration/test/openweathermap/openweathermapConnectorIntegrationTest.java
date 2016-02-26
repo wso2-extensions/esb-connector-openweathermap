d0d4229546e86f8496d78d903be352cf4a572a11
Shaki <Shaki@Shakis-MacBook-Pro.local>
2016-02-26 16:33:53 +0530

    Connector version is changed

--- src/test/java/org/wso2/carbon/connector/integration/test/openweathermap/openweathermapConnectorIntegrationTest.java
+++ src/test/java/org/wso2/carbon/connector/integration/test/openweathermap/openweathermapConnectorIntegrationTest.java
@@ -42,7 +42,7 @@ public class openweathermapConnectorIntegrationTest extends ConnectorIntegration
     @BeforeClass(alwaysRun = true)
     public void setEnvironment() throws Exception {
 
-        init("openweathermap-connector-1.0.0");
+        init("openweathermap-connector-1.0.1-SNAPSHOT");
         esbRequestHeadersMap.put("Accept-Charset", "UTF-8");
         esbRequestHeadersMap.put("Content-Type", "application/json");
         apiRequestHeadersMap.put("Content-Type", "application/x-www-form-urlencoded");
